534d5b5db9ac3afeb0964ee4c40b3e100eed41f8
Joey Lopez <joeyl509625@gmail.com>
2020-10-18 14:32:21 -0500

    added media queries

---
+++ src/components/Questions.js
@@ -0,0 +1,95 @@
+const Questions = [
+    {
+        questionText: 'What is Don Drapers go to Cocktail?', 
+        answerOptions: [
+            {answerText:'Old Fashioned', isCorrect:true}, 
+            {answerText:'Manhattan', isCorrect:false}, 
+            {answerText:'Sazerac', isCorrect:false}, 
+            {answerText:'Daquiri', isCorrect:false}
+        ]
+    },
+    {
+        questionText: 'This cocktail made a drastic resurgence due to the hit show "Sex in the City"', 
+        answerOptions: [
+            {answerText:'Margarita', isCorrect:false}, 
+            {answerText:'Cosmopolitan', isCorrect:true}, 
+            {answerText:'Hot Toddy', isCorrect:false}, 
+            {answerText:'Dirty Martini', isCorrect:false}
+        ]
+    },
+    {
+        questionText: '"Hey Bartender, know how to make a "___"? Famous words from Coughlin in the movie "Cocktail".', 
+        answerOptions: [
+            {answerText:'Whiskey Sour', isCorrect:false}, 
+            {answerText:'Manhattan', isCorrect:false}, 
+            {answerText:'Red Eye', isCorrect:true}, 
+            {answerText:'Mojito', isCorrect:false}
+        ]
+    },
+    {
+        questionText: 'Originally made with cognac.. this cocktail consists of champagne, gin and citrus.', 
+        answerOptions: [
+            {answerText:'Champagne Cocktail', isCorrect:false}, 
+            {answerText:'French Connection', isCorrect:false}, 
+            {answerText:'Aperol Spritz', isCorrect:false}, 
+            {answerText:'French 75', isCorrect:true}
+        ]
+    },
+    {
+        questionText: 'Though a little unorthodox, Bond orders this cocktail shaken.. not stirred', 
+        answerOptions: [
+            {answerText:'Vesper', isCorrect:true}, 
+            {answerText:'Gibson', isCorrect:false}, 
+            {answerText:'Dry Martini', isCorrect:false}, 
+            {answerText:'Manhattan', isCorrect:false}
+        ]
+    },
+    {
+        questionText: '" chacun prépare sa propre mort ", Which is a tradition in which the guest makes this cocktail.', 
+        answerOptions: [
+            {answerText:'French Martini', isCorrect:false}, 
+            {answerText:'Singapore Sling', isCorrect:false}, 
+            {answerText:'Ti Punch', isCorrect:true}, 
+            {answerText:'Press', isCorrect:false}
+        ]
+    },
+    {
+        questionText: 'What is the official drink of The Kentucy Derby ', 
+        answerOptions: [
+            {answerText:'Southside', isCorrect:false}, 
+            {answerText:'Mint Julep', isCorrect:true}, 
+            {answerText:'Brown Derby', isCorrect:false}, 
+            {answerText:'Moscow Mule', isCorrect:false}
+        ]
+    },
+    {
+        questionText: ' Made and founded in New Orleans.. this cocktail consists of Rye, Cognac, Absinthe and Peychaud bitters', 
+        answerOptions: [
+            {answerText:'Sazerac', isCorrect:true}, 
+            {answerText:'Highball', isCorrect:false}, 
+            {answerText:'Sour', isCorrect:false}, 
+            {answerText:'Apple Martini', isCorrect:false}
+        ]
+    },
+    
+    {
+        questionText: 'Japan perfected this elegant yet subtle cocktail', 
+        answerOptions: [
+            {answerText:'Gimlet', isCorrect:false}, 
+            {answerText:'Lychee Martini', isCorrect:false}, 
+            {answerText:'Highball', isCorrect:true}, 
+            {answerText:'Army Navy', isCorrect:false}
+        ]
+    },
+    {
+        questionText: '"Beer is for breakfast, drink for be gone!" Name the movie', 
+        answerOptions: [
+            {answerText:'The Hangover', isCorrect:false}, 
+            {answerText:'Cocktail', isCorrect:true}, 
+            {answerText:'The Big Lebowski', isCorrect:false}, 
+            {answerText:'The Gambler', isCorrect:false}
+        ]
+    },
+];
+
+export default Questions
